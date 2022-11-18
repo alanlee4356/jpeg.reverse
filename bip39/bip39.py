1726983c88519fecafc7922a17ea7841b44afa32
alanlee4356 <alanlee4356@gmail.com>
2022-11-18 16:38:18 +0900

    허프만테이블작성완료

--- bip39/bip39.py
+++ bip39/bip39.py
@@ -1,5 +1,38 @@
 import string
 import re
+from bidict import bidict
+
+HUFFMAN_CATEGORY_CODEWORD = bidict(
+    {
+
+        'e' : '100',
+        'a' : '0000',
+        'r' : '0001',
+        't' : '0011',
+        'o' : '0100',
+        'i' : '0101',
+        'n' : '0111',
+        's' : '1010',
+        'l' : '1100',
+        'c' : '1110',
+        'u' : '00100',
+        'd' : '01100',
+        'p' : '01101',
+        'm' : '10110',
+        'h' : '11010',
+        'g' : '11011',
+        'b' : '11111',
+        'y' : '001010',
+        'f' : '001011',
+        'w' : '101110',
+        'v' : '101111',
+        'k' : '111100',
+        'x' : '11110100',
+        'z' : '11110101',
+        'j' : '11110110',
+        'q' : '11110111'
+}
+    )
 
 
 def countalp(data):
@@ -28,6 +61,7 @@ alp = {}#alphabet
 countalp(data)
 sort_alp = sorted(alp.items(), key = lambda pair : pair[1], reverse=True)#빈도가 높은순으로 정렬
 print(sort_alp)
+print(sum(alp.values()))
 
 
 
